885e06bb7be7fb18b48de29af8d305af6e873da0
Danielle Turton <danielle.turton@ncl.ac.uk>
2017-11-15 15:20:56 +0000

    - created figures script

---
+++ tflap_figures.R
@@ -0,0 +1,23 @@
+## LOAD PACKAGES ####
+library(ggplot2)
+library(dplyr)
+
+theme_set(theme_bw())
+
+data_figs = data_clean
+
+ggplot(data_clean, aes(x = Position, fill = tvar)) +
+  geom_bar(position = "fill") + facet_wrap(~File)
+
+
+flap_fig_intervocalic = data_clean %>%
+  filter(Position == "Internal") %>%
+  filter(Code2 != "") %>%
+  #mutate(Group = factor(File, labels=c("Older", "Younger"))) %>%
+  mutate(Code2 = factor(Code2, labels=c("getting", "better"))) %>%
+  ggplot(aes(x = Code2, fill = tvar)) +
+  geom_bar(position = "fill") + facet_wrap(~File) +
+  ylab("") +
+  xlab("intervocalic context") +
+  scale_fill_brewer(palette = "Set1") +
+  theme(legend.title=element_blank(), legend.position="top")
