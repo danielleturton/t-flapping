23be1119ee681247eb3e5f1cafe72eb37452e183
Danielle Turton <danielle.turton@ncl.ac.uk>
2017-11-15 15:10:25 +0000

    - created cleaning script

---
+++ scripts/tflap_cleaning.R
@@ -0,0 +1,15 @@
+## LOAD PACKAGES ####
+library(ggplot2)
+library(dplyr)
+
+## LOAD DATA ####
+setwd("data/")
+  file_names <- dir() #where you have your files
+  data = do.call(rbind,lapply(file_names,read.delim))
+setwd("~/Dropbox/Projects/Blackburn/tflap/tflap_R")
+
+## CLEAN DATA ####
+data_clean = data %>%
+  mutate(tvar = recode(Code1, "d" = "f", "f?" = "f", "?f" = "f")) %>%
+  mutate(tvar = factor(tvar, labels=c("flap", "glottal", "t-to-r", "t", NA, "deleted", "?"))) %>%
+  filter(tvar == "flap" | tvar == "glottal" | tvar == "t")
