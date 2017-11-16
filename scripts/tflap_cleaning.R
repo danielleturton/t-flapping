74675a02f5f948fb9de1433b56b5ab9301713bf2
Danielle Turton <danielle.turton@ncl.ac.uk>
2017-11-16 10:29:27 +0000

    - added long/short vowels - added list of AA words

--- scripts/tflap_cleaning.R
+++ scripts/tflap_cleaning.R
@@ -8,11 +8,21 @@ setwd("data/")
 setwd("~/Dropbox/Projects/Blackburn/tflap/tflap_R")
 
 ## UNWANTED WORDS
-unwantedwords <- c("HOTEL", "HOTELS", "SETTEE", "SETTEES", "EIGHTEENTH")
+unwantedwords = c("HOTEL", "HOTELS", "SETTEE", "SETTEES", "EIGHTEENTH")
+
+## WORDS TO CHANGE CATEGORY
+#checking which words have Preceding AA
+filter(data_clean, Pre_Seg == "AA") %>%
+  distinct(Word)
+aawords = c("TOMATO", "TOMATOES", "*TOMATOEY")
 
 ## CLEAN DATA ####
 data_clean = data %>%
   mutate(tvar = recode(Code1, "d" = "f", "f?" = "f", "?f" = "f")) %>%
   mutate(tvar = factor(tvar, labels=c("flap", "glottal", "t-to-r", "t", NA, "deleted", "?"))) %>%
   filter(tvar == "flap" | tvar == "glottal" | tvar == "t") %>%
-  filter(!Word %in% unwantedwords)
+  filter(!Word %in% unwantedwords)%>%
+  mutate(vowel_length = recode(Pre_Seg, "AA" = "short", "AE" = "short", "AH" = "short", "AO" = "long", "AW" = "long", "AY" = "long",  "EH" = "short", "EY" = "long", "IH" = "short", "IY" = "long",  "OW" = "long",  "UH" = "short", "UW" = "long")) 
+ # need to change AA words with /a:/
+  
+
