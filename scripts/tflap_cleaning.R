f45e2900dba6c373f7226c97b9c47f011e6d1d4e
Danielle Turton <danielle.turton@ncl.ac.uk>
2017-11-17 10:41:08 +0000

    - added code to get rid of unwanted words in clean up

--- scripts/tflap_cleaning.R
+++ scripts/tflap_cleaning.R
@@ -22,7 +22,7 @@ data_clean = data %>%
   mutate(tvar = factor(tvar, labels=c("flap", "glottal", "t-to-r", "t", NA, "deleted", "?"))) %>%
   filter(tvar == "flap" | tvar == "glottal" | tvar == "t") %>%
   filter(!Word %in% unwantedwords)%>%
-  mutate(vowel_length = recode(Pre_Seg, "AA" = "short", "AE" = "short", "AH" = "short", "AO" = "long", "AW" = "long", "AY" = "long",  "EH" = "short", "EY" = "long", "IH" = "short", "IY" = "long",  "OW" = "long",  "UH" = "short", "UW" = "long")) 
- # need to change AA words with /a:/
+  mutate(vowel_length = recode(Pre_Seg, "AA" = "short", "AE" = "short", "AH" = "short", "AO" = "long", "AW" = "long", "AY" = "long",  "EH" = "short", "EY" = "long", "IH" = "short", "IY" = "long",  "OW" = "long",  "UH" = "short", "UW" = "long")) %>%
+  mutate(vowel_length = ifelse(Word %in% aawords, "long", as.character(vowel_length)))
   
 
