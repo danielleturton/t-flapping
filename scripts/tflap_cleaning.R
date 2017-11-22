13a03a8d3cc9e608ff8a8b31fa4f135f7337df2b
Danielle Turton <danielle.turton@ncl.ac.uk>
2017-11-22 15:40:47 +0000

    updated code and added in social data and organised columns

--- scripts/tflap_cleaning.R
+++ scripts/tflap_cleaning.R
@@ -21,13 +21,21 @@ filter(data_clean, Pre_Seg == "AA") %>%
   distinct(Word)
 aawords = c("TOMATO", "TOMATOES", "*TOMATOEY")
 
+#data = arrange(data, Code1)
+
+## Social data ####
+social = read.delim("/Users/danielle/Dropbox/Projects/Blackburn/Data/social_Nov17.csv")
+
 ## CLEAN DATA ####
 data_clean = data %>%
-  mutate(tvar = recode(Code1, "d" = "f", "f?" = "f", "?f" = "f")) %>%
-  mutate(tvar = factor(tvar, labels=c("flap", "glottal", "t-to-r", "t", NA, "deleted", "?"))) %>%
+  mutate(tvar = recode(Code1, "d" = "f", "f?" = "f", "?f" = "f", "tf" = "f")) %>%
+  mutate(tvar = factor(tvar, labels=c(NA, "deleted", "flap", "glottal", "t", "?", "tlat", "t-to-r"))) %>%
   filter(tvar == "flap" | tvar == "glottal" | tvar == "t") %>%
   filter(!Word %in% unwantedwords)%>%
   mutate(vowel_length = recode(Pre_Seg, "AA" = "short", "AE" = "short", "AH" = "short", "AO" = "long", "AW" = "long", "AY" = "long",  "EH" = "short", "EY" = "long", "IH" = "short", "IY" = "long",  "OW" = "long",  "UH" = "short", "UW" = "long")) %>%
-  mutate(vowel_length = ifelse(Word %in% aawords, "long", as.character(vowel_length)))
+  mutate(vowel_length = ifelse(Word %in% aawords, "long", as.character(vowel_length))) %>%
+  left_join(social) %>%  
+  mutate(duration = Seg_End - Seg_Start) %>% 
+  select(24, 21, 25:30, 3, 5, 11, 22, 14, 8, 39, 18)
   
 
